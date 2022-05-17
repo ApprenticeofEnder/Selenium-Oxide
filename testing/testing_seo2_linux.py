890a741b1451def51fa0207f0e3117ad2825a702
RobertEnderBabaev <rbabaev@reshiftsecurity.com>
2022-05-17 16:09:53 -0400

    start boilerplate for testing

--- testing/testing_seo2_linux.py
+++ testing/testing_seo2_linux.py
@@ -1,6 +1,9 @@
 import unittest
 
 from selenium_oxide.exploit_builder import ExploitBuilder
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 testfire_exploit = ExploitBuilder(
     "http",
@@ -21,15 +24,41 @@ xss_marker = "ECHO-ALPHA-TANGO"
 #         .send_enter_by_id("query")
 # )
 
+
 class TestFire(unittest.TestCase):
     def test_navigation(self):
         testfire_exploit.get("/")
-        self.assertEqual(testfire_driver.current_url, "http://demo.testfire.net/")
+        self.assertEqual(testfire_driver.current_url,
+                         "http://demo.testfire.net/")
 
     def test_content_extraction(self):
         testfire_exploit.get("/")
         login_txt = testfire_exploit.get_contents_by_id("LoginLink")
         self.assertEqual(login_txt, "Sign In")
 
-if __name__=='__main__':
+    def test_text_entry_by_id(self):
+        (
+            testfire_exploit
+            .get("/")
+            .type_by_id("query", xss_marker)
+        )
+        self.assertEqual(testfire_driver.find_element_by_id(
+            "query").get_attribute("value"), xss_marker)
+
+    def test_enter_by_id(self):
+        (
+            testfire_exploit
+            .get("/")
+            .type_by_id("query", xss_marker)
+            .send_enter_by_id("query")
+        )
+        WebDriverWait(testfire_driver, 2).until(
+            EC.text_to_be_present_in_element(
+                (By.TAG_NAME, 'p'),
+                xss_marker
+            )
+        )
+
+
+if __name__ == '__main__':
     unittest.main()
