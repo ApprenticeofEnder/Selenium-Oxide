5846653446c01761d9144d7eb29148c726ef80f0
Robert Babaev <perfknight@gmail.com>
2022-07-20 21:54:04 -0400

    update testing suite

--- testing/individual_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from selenium_oxide.exploit_builder import ExploitBuilder
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
