6805bd7ca752b9bb34060136c63a22fdf284278d
Robert Babaev <github@robertbabaev.tech>
2024-02-09 01:11:17 -0500

    formatting fixes

--- selenium_oxide/user_generator.py
+++ selenium_oxide/user_generator.py
@@ -1,5 +1,6 @@
 from faker import Faker
 
+
 class SeO2User:
     def __init__(self, gen: Faker = None, locale: str = "en_US", **data) -> None:
         self.gen = gen if gen else Faker(locale)
@@ -12,13 +13,13 @@ class SeO2User:
         else:
             name = self.gen.name_nonbinary()
         self.name = data.get("name", name)
-        self.first_name = self.name.split(' ')[0]
-        self.last_name = self.name.split(' ')[1]
+        self.first_name = self.name.split(" ")[0]
+        self.last_name = self.name.split(" ")[1]
         self.username = data.get("username", base_profile["username"])
         self.email = data.get("email", base_profile["mail"])
         self.address = data.get("address", base_profile["address"])
         self.birthdate = data.get("birthdate", base_profile["birthdate"])
-    
+
     def __str__(self) -> str:
         return f"{self.name} ({self.sex}), username {self.username}, email {self.email}, DOB {str(self.birthdate)}"
 
